0cbb2445ea2caf303bc541e6d36699251de55d9f
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-04-17 11:09:06 -0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
         {links.map((link) => (
             <li className='h-3/4 flex items-center justify-center relative' 
             key={link.hash}>
-              <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300', 
+              <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-300', 
               {'text-gray-950 dark:text-gray-200': activeSection === link.name,})} 
               href={link.hash}
               onClick={() => {
